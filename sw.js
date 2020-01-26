8a72ce4ded9f558e301cb6ab4b59eab7b37358e2
kazu719 <kazukitakahashi2000@jcom.zaq.ne.jp>
2020-01-26 21:58:39 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,50 @@
 self.addEventListener('fetch', function(){});
+
+var CACHE_NAME = cache-v1;
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+
+
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      
+        return cache.addAll(
+         "./index.html"
+         "./中国禅宗史.html"
+         "./工事中.html"
+         "./sw.js" 
+         "./manifest.json"
+         "./hotokelogo.ico"
+         "./assets/css/main.css"  
+        );
+      
+    })
+
+
+  );
+
+  self.addEventListener('activate', function(event) {  
+    event.waitUntil(
+      
+      caches.keys().then(function(cache) {
+        cache.map(function(name) {
+          if(CACHE_NAME !== name) caches.delete(name);
+        })
+      })
+      
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      
+      caches.match(event.request).then(function(res) {
+          if(res) return res;
+        
+          return fetch(event.request);
+      })
+      
+    );
+  });
